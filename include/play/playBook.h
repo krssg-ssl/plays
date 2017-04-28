a23a9484f5d2e770ba69d425f8810d6c8f8741c4
Mayank Bhushan <miku.bhushan@gmail.com>
2017-04-28 15:26:21 +0530

    first commit

---
+++ include/play/playBook.h
@@ -0,0 +1,40 @@
+#ifndef PLAY_BOOK_H
+#define PLAY_BOOK_H
+
+#include "krssg_ssl_msgs/BeliefState.h"
+
+// Forward Declarations
+namespace Strategy
+{
+  class BeliefState;
+  class Play;
+}
+
+namespace Strategy
+{
+  class PlayBook
+  {
+  public:
+    enum PlayID
+    {
+      None,
+      SetPosition, 
+      PassTest, 
+      TestPlay, 
+      MAX_PLAYS
+    };
+
+  protected:
+    const krssg_ssl_msgs::BeliefState& state;
+    Play*              playList[MAX_PLAYS];
+
+  public:
+    PlayBook(const krssg_ssl_msgs::BeliefState& state);
+    
+    ~PlayBook();
+
+    void reload(void);
+  }; // class PlayBook
+} // namespace Strategy
+
+#endif // PLAY_BOOK_H
