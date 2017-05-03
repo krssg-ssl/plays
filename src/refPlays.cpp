ec7c09d2744cde74f0e3d5bbd93a887b90b36fd0
kvmanohar22 <kvmanohar22@gmail.com>
2017-05-03 19:52:31 +0000

    Adding inline commenting

--- src/refPlays.cpp
+++ src/refPlays.cpp
@@ -1,6 +1,7 @@
 #include "refPlays.hpp"
 
 namespace Strategy{
+	// Halt Referee play function starts
 	HaltRP::HaltRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, HALT) {
 		
 		name = "Halt RefereePlay";
@@ -19,4 +20,7 @@ namespace Strategy{
 			roleList[i].push_back(std::make_pair("TStop", param));
 		}
 	}
+	// Halt Referee play functions end
+
+	// Add the remaining class functions here 
 }
