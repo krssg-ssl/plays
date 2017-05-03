f2e367707e744765cd96c1d263965c403034c080
kvmanohar22 <kvmanohar22@gmail.com>
2017-05-03 19:35:20 +0000

    Done

--- src/refPlays.cpp
+++ src/refPlays.cpp
@@ -1,7 +1,7 @@
 #include "refPlays.hpp"
 
 namespace Strategy{
-	HaltRP::HaltRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, Command.HALT) {
+	HaltRP::HaltRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, HALT) {
 		
 		name = "Halt RefereePlay";
 		assert(HomeTeam::SIZE == 6);
@@ -15,7 +15,7 @@ namespace Strategy{
 	void HaltRP::updateParam() {
 		Tactic::Param param;
 		for(int i=0;i<6;i++){
-			roleList[i].clear;
+			roleList[i].clear();
 			roleList[i].push_back(std::make_pair("TStop", param));
 		}
 	}
