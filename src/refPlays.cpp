ba1938e464086e66f958ca31c3d47ab51cbd3803
sipah00 <shubhamsipah00@gmail.com>
2017-05-07 19:18:43 +0530

    fixed code style

--- src/refPlays.cpp
+++ src/refPlays.cpp
@@ -1,162 +1,162 @@
 #include "refPlays.hpp"
 
-namespace Strategy{
-	// Halt Referee play function starts
-	HaltRP::HaltRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, HALT) {
-		
-		name = "Halt RefereePlay";
-		assert(HomeTeam::SIZE == 6);
+namespace Strategy {
+// Halt Referee play function starts
+HaltRP::HaltRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, HALT) {
 
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
+	name = "Halt RefereePlay";
+	assert(HomeTeam::SIZE == 6);
+
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
+}
 
-	void HaltRP::updateParam() {
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].clear();
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
+void HaltRP::updateParam() {
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].clear();
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
-    // Halt Referee play functions end
+}
+// Halt Referee play functions end
 
 
-    // TimeoutYellowRP  function starts---
-	TimeoutYellowRP::TimeoutYellowRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, TIMEOUT_YELLOW) {
-		
-		name = "TimeoutYellow RefereePlay";
-		assert(HomeTeam::SIZE == 6);
+// TimeoutYellowRP  function starts---
+TimeoutYellowRP::TimeoutYellowRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, TIMEOUT_YELLOW) {
 
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
+	name = "TimeoutYellow RefereePlay";
+	assert(HomeTeam::SIZE == 6);
+
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
+}
 
-	void TimeoutYellowRP::updateParam() {
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].clear();
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
+void TimeoutYellowRP::updateParam() {
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].clear();
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
-	// TimeoutYellowRP  function end
+}
+// TimeoutYellowRP  function end
 
-	// TimeoutBlueRP  function starts---
-	TimeoutBlueRP::TimeoutBlueRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, TIMEOUT_BLUE) {
-		
-		name = "TimeoutBlueRP RefereePlay";
-		assert(HomeTeam::SIZE == 6);
+// TimeoutBlueRP  function starts---
+TimeoutBlueRP::TimeoutBlueRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, TIMEOUT_BLUE) {
 
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
+	name = "TimeoutBlueRP RefereePlay";
+	assert(HomeTeam::SIZE == 6);
+
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
+}
 
-	void TimeoutBlueRP::updateParam() {
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].clear();
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
+void TimeoutBlueRP::updateParam() {
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].clear();
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
-	// TimeoutBlueRP  function end
+}
+// TimeoutBlueRP  function end
 
-	// GoalYellowRP  function starts---
-	GoalYellowRP::GoalYellowRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, GOAL_YELLOW) {
-		
-		name = "GoalYellow RefereePlay";
-		assert(HomeTeam::SIZE == 6);
+// GoalYellowRP  function starts---
+GoalYellowRP::GoalYellowRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, GOAL_YELLOW) {
 
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
+	name = "GoalYellow RefereePlay";
+	assert(HomeTeam::SIZE == 6);
+
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
+}
 
-	void GoalYellowRP::updateParam() {
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].clear();
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
+void GoalYellowRP::updateParam() {
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].clear();
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
-	// GoalYellowRP  function end
+}
+// GoalYellowRP  function end
 
-	// GoalBlueRP  function starts---
-	GoalBlueRP::GoalBlueRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, GOAL_BLUE) {
-		
-		name = "GoalBlue RefereePlay";
-		assert(HomeTeam::SIZE == 6);
+// GoalBlueRP  function starts---
+GoalBlueRP::GoalBlueRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, GOAL_BLUE) {
 
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
+	name = "GoalBlue RefereePlay";
+	assert(HomeTeam::SIZE == 6);
+
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
+}
 
-	void GoalBlueRP::updateParam() {
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].clear();
-			roleList[i].push_back(std::make_pair("TStop", param));   
-		}
+void GoalBlueRP::updateParam() {
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].clear();
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
-    // GoalBlueRP  function end
+}
+// GoalBlueRP  function end
 
-	// BallPlacementYellowRP  function starts---
-	BallPlacementYellowRP::BallPlacementYellowRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, BALL_PLACEMENT_YELLOW) {
-		
-		name = "BallPlacementYellow RefereePlay";
-		assert(HomeTeam::SIZE == 6);
+// BallPlacementYellowRP  function starts---
+BallPlacementYellowRP::BallPlacementYellowRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, BALL_PLACEMENT_YELLOW) {
 
-		Tactic::Param param;
-		for(int i=0;i<5;i++){
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
-		roleList[5].push_back(std::make_pair("TPassToPoint",param));	//param--where ball should be passed(designated area)
+	name = "BallPlacementYellow RefereePlay";
+	assert(HomeTeam::SIZE == 6);
 
-
+	Tactic::Param param;
+	for (int i = 0; i < 5; i++) {
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
+	roleList[5].push_back(std::make_pair("TPassToPoint", param));	//param--where ball should be passed(designated area)
 
-	void BallPlacementYellowRP::updateParam() {
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].clear();
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
-		roleList[5].push_back(std::make_pair("TPassToPoint",param));	//param--where ball should be passed(designated area)
+
+}
+
+void BallPlacementYellowRP::updateParam() {
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].clear();
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
-	// BallPlacementYellowRP  function end
+	roleList[5].push_back(std::make_pair("TPassToPoint", param));	//param--where ball should be passed(designated area)
+}
+// BallPlacementYellowRP  function end
 
-	// BallPlacementBlueRP  function starts---
-	BallPlacementBlueRP::BallPlacementBlueRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, BALL_PLACEMENT_BLUE) {
-		
-		name = "BallPlacementBlue RefereePlay";
-		assert(HomeTeam::SIZE == 6);
+// BallPlacementBlueRP  function starts---
+BallPlacementBlueRP::BallPlacementBlueRP(const krssg_ssl_msgs::BeliefState& state) : RefereePlay(state, BALL_PLACEMENT_BLUE) {
 
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
-		roleList[5].push_back(std::make_pair("TPassToPoint",param));   //param--where ball should be passed(designated area)
-	
+	name = "BallPlacementBlue RefereePlay";
+	assert(HomeTeam::SIZE == 6);
+
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
+	roleList[5].push_back(std::make_pair("TPassToPoint", param));  //param--where ball should be passed(designated area)
+
+}
 
 
-	void BallPlacementBlueRP::updateParam() {
-		Tactic::Param param;
-		for(int i=0;i<6;i++){
-			roleList[i].clear();
-			roleList[i].push_back(std::make_pair("TStop", param));
-		}
-		roleList[5].push_back(std::make_pair("TPassToPoint",param));	//param--where ball should be passed(designated area)
+void BallPlacementBlueRP::updateParam() {
+	Tactic::Param param;
+	for (int i = 0; i < 6; i++) {
+		roleList[i].clear();
+		roleList[i].push_back(std::make_pair("TStop", param));
 	}
-	// BallPlacementBlueRP  function end
-	
+	roleList[5].push_back(std::make_pair("TPassToPoint", param));	//param--where ball should be passed(designated area)
+}
+// BallPlacementBlueRP  function end
 
-	// Add the remaining class functions here 
+
+// Add the remaining class functions here
 }
